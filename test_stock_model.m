ff9ea62efcc49b35f11ee9677d27714853ae99ab
rhit-bakerjl1 <77908045+rhit-bakerjl1@users.noreply.github.com>
2023-04-25 10:52:09 -0400

    Tried to check claim about mu - sigma^2/2. Was unsuccessful.

--- test_stock_model.m
+++ test_stock_model.m
@@ -4,21 +4,31 @@ clc;
 
 % Constants
 P0  = 100;
-mu  = 0.03;
 sigma   = 0.15;
+% mu  = 0.03;
+mu  = 0.05*sigma^2/2;
 delT    = 1/260;
-days    = 260*3;
+days    = 260*50;
 
-% Plotting and finding new stuff
-for plt = 1:2
-    figure(plt);
-    clf;
-    hold on;
-    for i = 1:5
-        % Find new random stonks vector
-        [t_vec, P_vec] = stock_model(P0, mu, sigma, delT, days);
-        plot(t_vec, P_vec, 'LineWidth', 1);
-    end
-    xlabel("time (day)");
-    ylabel("Stock Price");
+% Finding average return
+num_sim     = 100000;
+Pf_vec  = 1:num_sim;
+for i = 1:num_sim
+    [t_vec, P_vec] = stock_model(P0, mu, sigma, delT, days);
+    Pf_vec(i) = P_vec(end);
 end
+disp("Average value after 20 years is: " + mean(Pf_vec));
+
+% Plots
+% for plt = 1:2
+%     figure(plt);
+%     clf;
+%     hold on;
+%     for i = 1:5
+%         % Find new random stonks vector
+%         [t_vec, P_vec] = stock_model(P0, mu, sigma, delT, days);
+%         plot(t_vec, P_vec, 'LineWidth', 1);
+%     end
+%     xlabel("time (day)");
+%     ylabel("Stock Price");
+% end
