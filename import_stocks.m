6b78b68276ff0f8620222ce1a9bad8438a8fa9cb
kattmabm <kattmabm@rose-hulman.edu>
2023-05-02 10:36:50 -0400

    Corrected volatility calculation

--- import_stocks.m
+++ import_stocks.m
@@ -2,19 +2,29 @@ function [history, names, growth_rate, volatility, covariance] = import_stocks(f
 % Parses the data in the inputted .csv file. Returns the stock price
 % history, stock names, annual growth rates, and volatilities.
 
+% Constant values used
+DAYS_PER_YEAR = 365.25;
 % Read stock prices from a .csv generated from scrape_stocks.py
 stocks = readtable(filepath);
 % Extract the stock price names
 names = stocks.Properties.VariableNames(2:end);
+% Get the duration of time being used
 times = table2array(stocks(:,1));
 duration = between(times(1), times(end));
 years = calyears(duration) + (calmonths(duration)/12) + (caldays(duration)/365);
+% Extract the stock price history into an array
 history = table2array(stocks(:,2:end));
+% Extract the size of our stock history
+% Rows = number of days, cols = number of stocks
+[rows, cols] = size(history);
+% Get the time step delta_t
+delta_t = years * DAYS_PER_YEAR / rows;
+% Calculate the covariance of stocks from the price history
 covariance = cov(history);
-[~, cols] = size(history);
+% Calculate growth rate and volatility of each stock
 growth_rate = zeros(1, cols);
 volatility = zeros(1, cols);
 for i = 1 : cols
     growth_rate(i) = (history(end, i) - history(1, i))*years/history(1, i);
-    volatility(i) = sqrt(sse(history(:,i)));
+    volatility(i) = sqrt(covariance(i,i))/delta_t;
 end
